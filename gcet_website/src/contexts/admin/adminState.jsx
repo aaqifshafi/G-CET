f06c89a28d95db5b3f63235fd8094e1db11958bd
aaqifshafi <aaqifshafi@gmail.com>
2024-04-13 15:52:47 +0530

    added admin context

---
+++ gcet_website/src/contexts/admin/adminState.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import adminContext from "./adminContext";
+import { useRouter } from "next/navigation";
+const apiURL = process.env.NEXT_PUBLIC_API_HOST;
+
+const AdminState = (props) => {
+  const router = useRouter();
+
+  const [isUserLogin, setIsUserLogin] = useState(false);
+  const [adminDetails, setAdminDetails] = useState({});
+  const [dataFetching, setDataFetching] = useState(true);
+
+  const fetchData = async () => {
+    try {
+      const authorization =
+        sessionStorage.getItem("authorization") ||
+        localStorage.getItem("authorization");
+      if (!authorization) {
+        router.replace("/admin/login");
+        return;
+      }
+
+      const option = {
+        method: "GET",
+        headers: {
+          authorization,
+        },
+      };
+
+      const res = await fetch(`${apiURL}/admin/verifyAdmin`, option);
+      const data = await res.json();
+      if (data.success) {
+        setDataFetching(false);
+        setIsUserLogin(true);
+        setAdminDetails(data.admin);
+      } else {
+        setIsUserLogin(false);
+        localStorage.removeItem("authorization");
+        sessionStorage.removeItem("authorization");
+        router.replace("/admin/login");
+      }
+    } catch (err) {
+      console.error("Error fetching data:", err);
+      setIsUserLogin(false);
+      router.replace("/admin/login");
+    }
+  };
+
+  return (
+    <adminContext.Provider
+      value={{ isUserLogin, adminDetails, dataFetching, fetchData }}
+    >
+      {/* eslint-disable-next-line react/prop-types */}
+      {props.children}
+    </adminContext.Provider>
+  );
+};
+
+export default AdminState;
