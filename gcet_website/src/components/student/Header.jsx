ab7fa398709b49ffb616893d691a7e7ece63f6b2
aaqifshafi <aaqifshafi@gmail.com>
2024-04-13 15:41:00 +0530

    Fix header links in student Header component

--- gcet_website/src/components/student/Header.jsx
+++ gcet_website/src/components/student/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-br from-blue-950 to-blue-900 p-3 shadow-md">
       <nav className="max-w-screen-xl mx-auto my-3 flex items-center gap-8 flex-col justify-center sm:flex-row sm:justify-between">
-        <Link href={'/'}>
+        <Link href={"/"}>
           <Image
             className="invert"
             src="/logo.webp"
@@ -16,10 +16,9 @@ const Header = () => {
         </Link>
         <ul className="flex gap-8 text-white">
           <li>
-            <Link className="hover:underline duration-500" href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link className="hover:underline duration-500" href={""}>Contact</Link>
+            <Link className="hover:underline duration-500" href={"/"}>
+              Home
+            </Link>
           </li>
         </ul>
       </nav>
