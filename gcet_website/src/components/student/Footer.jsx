51b548e6c25e4eac6dd51ff3fb9b9e003465d312
aaqifshafi <aaqifshafi@gmail.com>
2024-04-25 13:32:10 +0530

    Update footer and header components

--- gcet_website/src/components/student/Footer.jsx
+++ gcet_website/src/components/student/Footer.jsx
@@ -2,7 +2,7 @@ import Heading2 from "@/components/Heading2";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-bl from-blue-950 to-blue-900 p-3"></footer>
+    <footer className="bg-gradient-to-bl from-blue-950 to-blue-900 p-10"></footer>
   );
 };
 
