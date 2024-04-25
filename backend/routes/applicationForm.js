d784ae4b0ecb9d4fa55de4fb56f1de36d7c8a316
aaqifshafi <aaqifshafi@gmail.com>
2024-04-25 20:22:26 +0530

    Refactor applicationForm.js to improve form submission logic

--- backend/routes/applicationForm.js
+++ backend/routes/applicationForm.js
@@ -8,16 +8,16 @@ router.post('/student/form', async (req, res) => {
     try {
         // Destructure the required properties from req.body
         const formData = req.body;
-
         const { email, enrollmentNumber, currentSem } = formData;
 
-        //Try  Updating the student document in the database
+        // Try to find the student document in the database
         const filter = { email: email };
         const student = await Students.findOne(filter);
+
         if (!student) {
             // If no student found with the provided email, return a 404 response
-            return res.status(404).json({ message: 'No student registered the provided email', success: false });
-        } else if (student) {
+            return res.status(404).json({ message: 'No student registered with the provided email', success: false });
+        } else {
             // Check if a form with the same enrollmentNumber and currentSem already exists
             const existingForm = await Form.findOne({ enrollmentNumber, currentSem });
             if (existingForm) {
@@ -32,17 +32,20 @@ router.post('/student/form', async (req, res) => {
                 const newForm = new Form(data);
                 // Save the new form document
                 const savedForm = await newForm.save();
+
                 // Update the student document to add the reference to the new form
                 student.forms.push(savedForm._id);
                 await student.save();
-                return res.status(200).json({ success: true, message: 'Form successfully submitted' });
 
+                // Populate the forms field before sending the response
+                const populatedStudent = await Students.findOne(filter).populate('forms')
+
+
+
+
+                return res.status(200).json({ success: true, message: 'Form successfully submitted', student: populatedStudent });
             }
-
-
         }
-
-
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal server error', success: false });
