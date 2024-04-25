94944d0f4373aeb89da336e264a65aab619a6627
aaqifshafi <aaqifshafi@gmail.com>
2024-04-25 15:01:32 +0530

    Final Footer Change

--- gcet_website/src/components/student/Footer.jsx
+++ gcet_website/src/components/student/Footer.jsx
@@ -1,8 +1,12 @@
-import Heading2 from "@/components/Heading2";
+import CopyWriteSection from "../public/footer/CopyWriteSection";
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-bl from-blue-950 to-blue-900 p-10"></footer>
+    <footer className="bg-gradient-to-bl from-blue-950 to-blue-900">
+      <div className="max-w-screen-xl mx-auto">
+        <CopyWriteSection />
+      </div>
+    </footer>
   );
 };
 
